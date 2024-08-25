9054406d99345a2e4ae0f66cb0b97e808c27789b
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-08-25 21:05:19 +0545

    Create toStringMethod.java
    Array.toString() Method.

---
+++ stage002_Array&ArrayList/Documentation/toStringMethod.java
@@ -0,0 +1,13 @@
+
+//Array.toString() Methord
+import java.util.Arrays;
+
+public class toStringMethod{
+    
+    public static void main(String[] args) {
+        int[] arry = {1, 2, 3, 4};
+        
+        System.out.print(Arrays.toString(arry));//A Arrays class that helps ro print an arry directly
+    }
+    
+}
