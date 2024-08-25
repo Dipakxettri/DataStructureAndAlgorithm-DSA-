f85d4190d27a8a90cbc158c2c976a49243460409
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-08-25 20:24:40 +0545

    Update 1DArrayInputOutput.java
    1D Array Input Output.

--- stage002_Array&ArrayList/Documentation/1DArrayInputOutput.java
+++ stage002_Array&ArrayList/Documentation/1DArrayInputOutput.java
@@ -1,1 +1,24 @@
 
+//1D Array Input Output
+  
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+       int [ ] arry = new int[4];//Arry object of size 4 the defult value is 0
+        
+        //input:
+        for(int i = 0; i<arry.length; i++){
+            arry[i] = sc.nextInt();
+        }
+        
+        //Output:
+        for(int j = 0; j<arry.length; j++){
+        System.out.print(arry[j] + " ");
+    }
+    }
+}
+
